2d3622b672ea38a3bb37f0b9517f9f9a11f8c2ba
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-08-21 00:18:03 +0300

    update SMSAero.Utils module to build with GHC 7.8

--- src/SMSAero/Utils.hs
+++ src/SMSAero/Utils.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module SMSAero.Utils where
 
+import Control.Applicative
 import Servant.API.Alternative
 
 -- | Distribute a client looking like
@@ -27,7 +28,7 @@ instance DistributiveClient (a -> b) (a -> b) where
   distributeClient = id
 
 instance (DistributiveClient (a -> b) b', DistributiveClient (a -> c) c') => DistributiveClient (a -> (b :<|> c)) (b' :<|> c') where
-  distributeClient client = distributeClient (aleft <$> client) :<|> distributeClient (aright <$> client)
+  distributeClient client = distributeClient (left <$> client) :<|> distributeClient (right <$> client)
     where
-      aleft  (l :<|> _) = l
-      aright (_ :<|> r) = r
+      left  (l :<|> _) = l
+      right (_ :<|> r) = r
