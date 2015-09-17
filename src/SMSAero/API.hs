71492ece3654f7cdcbaf1168d68c0219b2104656
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2015-09-17 14:17:04 +0300

    Provide instances for automatic API documentation

--- src/SMSAero/API.hs
+++ src/SMSAero/API.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -46,7 +47,8 @@ module SMSAero.API (
 import Data.Aeson
 import Data.Proxy
 
-import Data.Time (UTCTime)
+import Data.Time (UTCTime(UTCTime))
+import Data.Time.Calendar (fromGregorian)
 import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds, posixSecondsToUTCTime)
 
 import Data.Text (Text)
@@ -58,6 +60,8 @@ import Text.Read (readMaybe)
 
 import Servant.API
 import Servant.Client
+import Servant.Docs
+import Control.Lens (over, (|>))
 
 import GHC.Generics
 
@@ -70,12 +74,23 @@ instance Accept SmsAeroJson where
 instance FromJSON a => MimeUnrender SmsAeroJson a where
   mimeUnrender _ = mimeUnrender (Proxy :: Proxy JSON)
 
+instance ToJSON a => MimeRender SmsAeroJson a where
+  mimeRender _ = mimeRender (Proxy :: Proxy JSON)
+
 -- | Like 'QueryParam', but always required.
 data RequiredQueryParam (sym :: Symbol) a
 
 instance (HasClient sub, KnownSymbol sym, ToText a) => HasClient (RequiredQueryParam sym a :> sub) where
   type Client (RequiredQueryParam sym a :> sub) = a -> Client sub
   clientWithRoute _ req baseurl param = clientWithRoute (Proxy :: Proxy (QueryParam sym a :> sub)) req baseurl (Just param)
+
+instance (KnownSymbol sym, ToParam (QueryParam sym a), HasDocs sub) => HasDocs (RequiredQueryParam sym a :> sub) where
+  docsFor _ (endpoint, action) =
+    docsFor subP (endpoint, action')
+
+    where subP = Proxy :: Proxy sub
+          paramP = Proxy :: Proxy (QueryParam sym a)
+          action' = over params (|> toParam paramP) action
 
 -- | SMSAero sender's signature. This is used for the "from" field.
 newtype Signature = Signature { getSignature :: Text } deriving (Show, FromJSON, ToJSON, ToText, FromText)
@@ -130,12 +145,37 @@ instance HasClient sub => HasClient (RequireAuth :> sub) where
       authUser
       authPassword
 
+instance HasDocs sub => HasDocs (RequireAuth :> sub) where
+  docsFor _ (endpoint, action) =
+    docsFor subP (endpoint, action')
+
+    where subP = Proxy :: Proxy sub
+          userP = DocQueryParam "user"
+                    ["alice@example.com", "bob@example.com"]
+                    "SMSAero username (email) for authentication."
+                    Normal
+          passP = DocQueryParam "password"
+                    ["5f4dcc3b5aa765d61d8327deb882cf99", "d8578edf8458ce06fbc5bb76a58c5ca4"]
+                    "MD5 hash of a password."
+                    Normal
+          action' = over params ((|> passP) . (|> userP)) action
+
 -- | Implicit parameter that tells SMSAero to respond with JSON.
 data AnswerJson
 
 instance HasClient sub => HasClient (AnswerJson :> sub) where
     type Client (AnswerJson :> sub) = Client sub
     clientWithRoute _ req baseurl = clientWithRoute (Proxy :: Proxy (RequiredQueryParam "answer" Text :> sub)) req baseurl "json"
+
+instance HasDocs sub => HasDocs (AnswerJson :> sub) where
+  docsFor _ (endpoint, action) = docsFor subP (endpoint, action')
+    where
+      subP = Proxy :: Proxy sub
+      answerP = DocQueryParam "answer"
+                  ["json"]
+                  "When present makes SMSAero REST API to respond with JSON."
+                  Normal
+      action' = over params (|> answerP) action
 
 -- | Regular SMSAero GET API.
 type SmsAeroGet a = Get '[SmsAeroJson] (SmsAeroResponse a)
@@ -156,8 +196,38 @@ type SendApi =
   QueryParam "date" SMSAeroDate :>
   SmsAeroGet SendResponse
 
+instance ToParam (QueryParam "to" Phone) where
+  toParam _ = DocQueryParam "to"
+                ["74951234567"]
+                "Recipient phone number."
+                Normal
+
+instance ToParam (QueryParam "text" Text) where
+  toParam _ = DocQueryParam "text"
+                ["Hello, world!"]
+                "Message content."
+                Normal
+
+instance ToParam (QueryParam "from" Signature) where
+  toParam _ = DocQueryParam "from"
+                ["My Company"]
+                "Sender's signature."
+                Normal
+
+instance ToParam (QueryParam "date" SMSAeroDate) where
+  toParam _ = DocQueryParam "date"
+                [show (utcTimeToPOSIXSeconds (UTCTime (fromGregorian 2015 01 31) 0))]
+                "Requested datetime of delivery as number of seconds since 01 Jan 1970."
+                Normal
+
 -- | SMSAero API to check message status.
 type StatusApi = RequiredQueryParam "id" MessageId :> SmsAeroGet StatusResponse
+
+instance ToParam (QueryParam "id" MessageId) where
+  toParam _ = DocQueryParam "id"
+                ["12345"]
+                "Message ID, returned previously by SMSAero."
+                Normal
 
 -- | Every SMSAero response is either rejected or provides some info.
 data SmsAeroResponse a
@@ -175,6 +245,9 @@ data SendResponse
 -- | This is a generic instance and __does not match__ @FromJSON@.
 instance ToJSON SendResponse
 
+instance ToSample (SmsAeroResponse SendResponse) (SmsAeroResponse SendResponse) where
+  toSample _ = Nothing
+
 -- | SMSAero response to a status request.
 data StatusResponse
   = StatusDeliverySuccess   -- ^ Message is successfully delivered.
@@ -187,13 +260,22 @@ data StatusResponse
 -- | This is a generic instance and __does not match__ @FromJSON@.
 instance ToJSON StatusResponse
 
+instance ToSample (SmsAeroResponse StatusResponse) (SmsAeroResponse StatusResponse) where
+  toSample _ = Nothing
+
 -- | SMSAero response to a balance request.
 -- This is a number of available messages to send.
 newtype BalanceResponse = BalanceResponse Double deriving (Show, ToJSON)
 
+instance ToSample (SmsAeroResponse BalanceResponse) (SmsAeroResponse BalanceResponse) where
+  toSample _ = Nothing
+
 -- | SMSAero response to a senders request.
 -- This is just a list of available signatures.
 newtype SendersResponse = SendersResponse [Signature] deriving (Show, FromJSON, ToJSON)
+
+instance ToSample (SmsAeroResponse SendersResponse) (SmsAeroResponse SendersResponse) where
+  toSample _ = Nothing
 
 -- | SMSAero response to a sign request.
 data SignResponse
@@ -203,6 +285,9 @@ data SignResponse
   deriving (Show, Generic)
 -- | This is a generic instance and __does not match__ @FromJSON@.
 instance ToJSON SignResponse
+
+instance ToSample (SmsAeroResponse SignResponse) (SmsAeroResponse SignResponse) where
+  toSample _ = Nothing
 
 instance FromJSON a => FromJSON (SmsAeroResponse a) where
   parseJSON (Object o) = do
